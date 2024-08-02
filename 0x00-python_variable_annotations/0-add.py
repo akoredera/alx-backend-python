9e30ea6f676e2e9933a71e764e29f0c87b624132
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-02 16:22:41 +0000

    addpython  files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+add function using typing format
+'''
+
+
+def add(a: float, b: float) -> float:
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from math import floor
+'''
+a type-annotated function floor which takes
+a float n as argument and returns the floor of the float.
+'''
+
+
+def floor(n: float) -> int:
+    return int(n)
