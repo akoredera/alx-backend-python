8256c12164b22730c49acb6bad8f6648a557bac6
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-09 22:37:08 +0000

    modified task 1 file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,9 @@
 1. Async Comprehensions
 '''
 
-async_generator = __import__('0-async_generator').async_generator
 import asyncio
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
